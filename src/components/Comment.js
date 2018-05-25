c9c9dffc729c91eb5d20e9fca0dfbd21dd6511fe
Tim Hamrouge <Tim@Tims-Mac-mini.local>
2018-05-25 11:44:46 +0100

    styled date displays on comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Voter from "./Voter";
+import moment from "moment";
 
 class Comment extends React.Component {
   render() {
     let comment = this.props.comment;
+    let date = moment(comment.created_at).fromNow();
     return (
       <div className="comment">
         <li className="list-group-item border-0">
@@ -16,7 +18,7 @@ class Comment extends React.Component {
                 <div className="card-body">
                   <h5>{comment.body}</h5>
                   <small>
-                    Posted by: {comment.created_by} on {comment.created_at}
+                    Posted by: {comment.created_by} {date}
                   </small>
                 </div>
               </div>
